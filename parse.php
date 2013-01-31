f9f59ffb61936ce5c43b109f74b0adaa8abd57a0
Jon Cave <jon@joncave.co.uk>
2013-01-31 20:11:48 +0000

    Use phpdocumentor/reflection to parse files

--- parse.php
+++ parse.php
@@ -1,16 +1,19 @@
 <?php
 
-require dirname(__FILE__) . '/PHP-Parser/lib/PHPParser/Autoloader.php';
-PHPParser_Autoloader::register();
-require dirname(__FILE__) . '/library/WP/NodeVisitor.php';
+require 'vendor/autoload.php';
 
-$wp_dir = dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . 'trunk-git' . DIRECTORY_SEPARATOR . 'wp-includes';
+use phpDocumentor\Reflection\FileReflector;
+
+if ($_SERVER['argc'] < 2)
+	die("Please provide a directory to scan.\n");
+
+$wp_dir = $_SERVER['argv'][1];
 
 function get_wp_files($directory) {
 	$iterableFiles =  new RecursiveIteratorIterator(
 		new RecursiveDirectoryIterator($directory)
-    );
-    $files = array();
+	);
+	$files = array();
 	try {
 		foreach( $iterableFiles as $file ) {
 			if ($file->getExtension() !== 'php')
@@ -29,42 +32,30 @@ function get_wp_files($directory) {
 	return $files;
 }
 
-function parse_files($files) {
-	header('Content-Type: text/plain');
+function parse_files($files, $root) {
+	$functions = array();
 
-	$parser = new PHPParser_Parser(new PHPParser_Lexer);
-	$repository = new QP_Repo_Functions;
-	$filters = new QP_Repo_Filters;
+	foreach ($files as $filename) {
+		$file = new FileReflector($filename);
 
-	foreach ($files as $file) {
-		$code = file_get_contents($file);
-		$file = str_replace($wp_dir . DIRECTORY_SEPARATOR, '', $file);
-		$file = str_replace(DIRECTORY_SEPARATOR, '/', $file);
-		try {
-			$stmts = $parser->parse($code);
+		$path = ltrim(substr($filename, strlen($root)), DIRECTORY_SEPARATOR);
+		$file->setFilename($path);
+
+		$file->process();
+
+		// TODO proper exporter
+		foreach ($file->getFunctions() as $function) {
+			$functions[$file->getFilename()][] = array(
+				'name' => $function->getShortName(),
+				'line' => $function->getLineNumber(),
+			);
 		}
-		catch (PHPParser_Error $e) {
-			echo $file . "\n";
-			echo $e->getMessage();
-			die();
-		}
-
-		$traverser = new PHPParser_NodeTraverser;
-		$visitor = new QP_NodeVisitor($file, $repository, $filters);
-		$traverser->addVisitor($visitor);
-		$stmts = $traverser->traverse($stmts);
 	}
 
-	$functions = array_filter($repository->functions, function ($details) {
-		// Built-in function
-		return !empty($details->file) || !empty($details->uses);
-	});
-
-	return array('functions' => $functions, 'filters' => $filters->filters);
+	return $functions;
 }
 
 $files = get_wp_files($wp_dir);
-list($functions, $filters) = parse_files($files);
+$functions = parse_files($files, $wp_dir);
 
 file_put_contents('output.json', json_encode($functions));
-file_put_contents('filters.json', json_encode($filters));
