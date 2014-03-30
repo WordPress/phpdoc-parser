14b1271a6aa8576f1258b84e7692d4da6471f345
JDGrimes <jdg@codesymphony.co>
2014-03-30 16:20:06 -0400

    Remove a redundant class constructor

--- lib/class-function-call-reflector.php
+++ lib/class-function-call-reflector.php
@@ -15,19 +15,6 @@ use phpDocumentor\Reflection\DocBlock\Context;
 class Function_Call_Reflector extends BaseReflector {
 
 	/**
-	 * Initializes the reflector using the function statement object of
-	 * PHP-Parser.
-	 *
-	 * @param \PHPParser_Node_Expr_FuncCall              $node    Function object
-	 *                                                            coming from PHP-Parser.
-	 * @param \phpDocumentor\Reflection\DocBlock\Context $context The context in
-	 *                                                            which the node occurs.
-	 */
-	public function __construct( \PHPParser_Node_Expr $node, Context $context ) {
-		parent::__construct( $node, $context );
-	}
-
-	/**
 	 * Returns the name for this Reflector instance.
 	 *
 	 * @return string
