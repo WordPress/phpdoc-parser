6047cad8e3e3ff33786557b7c1bea573baa0c113
Anton Timmermans <me@atimmer.com>
2014-10-23 23:13:52 +0200

    Fix file indentation

--- lib/class-file-reflector.php
+++ lib/class-file-reflector.php
@@ -1,241 +1,241 @@
 <?php
 
-	namespace WP_Parser;
+namespace WP_Parser;
 
-	use phpDocumentor\Reflection;
-	use phpDocumentor\Reflection\FileReflector;
+use phpDocumentor\Reflection;
+use phpDocumentor\Reflection\FileReflector;
+
+/**
+ * Reflection class for a full file.
+ *
+ * Extends the FileReflector from phpDocumentor to parse out WordPress
+ * hooks and note function relationships.
+ */
+class File_Reflector extends FileReflector {
+	/**
+	 * List of elements used in global scope in this file, indexed by element type.
+	 *
+	 * @var array {
+	 *      @type \WP_Parser\Hook_Reflector[]          $hooks     The action and filters.
+	 *      @type \WP_Parser\Function_Call_Reflector[] $functions The functions called.
+	 * }
+	 */
+	public $uses = array();
 
 	/**
-	 * Reflection class for a full file.
+	 * List of elements used in the current node scope, indexed by element type.
 	 *
-	 * Extends the FileReflector from phpDocumentor to parse out WordPress
-	 * hooks and note function relationships.
+	 * @var array {@see \WP_Parser\File_Reflector::$uses}
 	 */
-	class File_Reflector extends FileReflector {
-		/**
-		 * List of elements used in global scope in this file, indexed by element type.
-		 *
-		 * @var array {
-		 *      @type \WP_Parser\Hook_Reflector[]          $hooks     The action and filters.
-		 *      @type \WP_Parser\Function_Call_Reflector[] $functions The functions called.
-		 * }
-		 */
-		public $uses = array();
+	protected $uses_queue = array();
 
-		/**
-		 * List of elements used in the current node scope, indexed by element type.
-		 *
-		 * @var array {@see \WP_Parser\File_Reflector::$uses}
-		 */
-		protected $uses_queue = array();
+	/**
+	 * List of elements used in the current class scope, indexed by method.
+	 *
+	 * @var array[][] {@see \WP_Parser\File_Reflector::$uses}
+	 */
+	protected $method_uses_queue = array();
 
-		/**
-		 * List of elements used in the current class scope, indexed by method.
-		 *
-		 * @var array[][] {@see \WP_Parser\File_Reflector::$uses}
-		 */
-		protected $method_uses_queue = array();
+	/**
+	 * Stack of classes/methods/functions currently being parsed.
+	 *
+	 * @see \WP_Parser\FileReflector::getLocation()
+	 * @var \phpDocumentor\Reflection\BaseReflector[]
+	 */
+	protected $location = array();
 
-		/**
-		 * Stack of classes/methods/functions currently being parsed.
-		 *
-		 * @see \WP_Parser\FileReflector::getLocation()
-		 * @var \phpDocumentor\Reflection\BaseReflector[]
-		 */
-		protected $location = array();
+	/**
+	 * Last DocBlock associated with a non-documentable element.
+	 *
+	 * @var \PHPParser_Comment_Doc
+	 */
+	protected $last_doc = null;
 
-		/**
-		 * Last DocBlock associated with a non-documentable element.
-		 *
-		 * @var \PHPParser_Comment_Doc
-		 */
-		protected $last_doc = null;
+	/**
+	 * Add hooks to the queue and update the node stack when we enter a node.
+	 *
+	 * If we are entering a class, function or method, we push it to the location
+	 * stack. This is just so that we know whether we are in the file scope or not,
+	 * so that hooks in the main file scope can be added to the file.
+	 *
+	 * We also check function calls to see if there are any actions or hooks. If
+	 * there are, they are added to the file's hooks if in the global scope, or if
+	 * we are in a function/method, they are added to the queue. They will be
+	 * assigned to the function by leaveNode(). We also check for any other function
+	 * calls and treat them similarly, so that we can export a list of functions
+	 * used by each element.
+	 *
+	 * Finally, we pick up any docblocks for nodes that usually aren't documentable,
+	 * so they can be assigned to the hooks to which they may belong.
+	 */
+	public function enterNode( \PHPParser_Node $node ) {
+		parent::enterNode( $node );
 
-		/**
-		 * Add hooks to the queue and update the node stack when we enter a node.
-		 *
-		 * If we are entering a class, function or method, we push it to the location
-		 * stack. This is just so that we know whether we are in the file scope or not,
-		 * so that hooks in the main file scope can be added to the file.
-		 *
-		 * We also check function calls to see if there are any actions or hooks. If
-		 * there are, they are added to the file's hooks if in the global scope, or if
-		 * we are in a function/method, they are added to the queue. They will be
-		 * assigned to the function by leaveNode(). We also check for any other function
-		 * calls and treat them similarly, so that we can export a list of functions
-		 * used by each element.
-		 *
-		 * Finally, we pick up any docblocks for nodes that usually aren't documentable,
-		 * so they can be assigned to the hooks to which they may belong.
-		 */
-		public function enterNode( \PHPParser_Node $node ) {
-			parent::enterNode( $node );
+		switch ( $node->getType() ) {
+			// Add classes, functions, and methods to the current location stack
+			case 'Stmt_Class':
+			case 'Stmt_Function':
+			case 'Stmt_ClassMethod':
+				array_push( $this->location, $node );
+				break;
 
-			switch ( $node->getType() ) {
-				// Add classes, functions, and methods to the current location stack
-				case 'Stmt_Class':
-				case 'Stmt_Function':
-				case 'Stmt_ClassMethod':
-					array_push( $this->location, $node );
-					break;
+			// Parse out hook definitions and function calls and add them to the queue.
+			case 'Expr_FuncCall':
+				$function = new \WP_Parser\Function_Call_Reflector( $node, $this->context );
 
-				// Parse out hook definitions and function calls and add them to the queue.
-				case 'Expr_FuncCall':
-					$function = new \WP_Parser\Function_Call_Reflector( $node, $this->context );
+				/*
+				 * If the function call is in the global scope, add it to the
+				 * file's function calls. Otherwise, add it to the queue so it
+				 * can be added to the correct node when we leave it.
+				 */
+				if ( $this === $this->getLocation() ) {
+					$this->uses['functions'][] = $function;
+				} else {
+					$this->uses_queue['functions'][] = $function;
+				}
+
+				if ( $this->isFilter( $node ) ) {
+					if ( $this->last_doc && ! $node->getDocComment() ) {
+						$node->setAttribute( 'comments', array( $this->last_doc ) );
+						$this->last_doc = null;
+					}
+
+					$hook = new \WP_Parser\Hook_Reflector( $node, $this->context );
 
 					/*
-					 * If the function call is in the global scope, add it to the
-					 * file's function calls. Otherwise, add it to the queue so it
-					 * can be added to the correct node when we leave it.
+					 * If the hook is in the global scope, add it to the file's
+					 * hooks. Otherwise, add it to the queue so it can be added to
+					 * the correct node when we leave it.
 					 */
 					if ( $this === $this->getLocation() ) {
-						$this->uses['functions'][] = $function;
+						$this->uses['hooks'][] = $hook;
 					} else {
-						$this->uses_queue['functions'][] = $function;
+						$this->uses_queue['hooks'][] = $hook;
 					}
+				}
+				break;
 
-					if ( $this->isFilter( $node ) ) {
-						if ( $this->last_doc && ! $node->getDocComment() ) {
-							$node->setAttribute( 'comments', array( $this->last_doc ) );
-							$this->last_doc = null;
-						}
+			// Parse out method calls, so we can export where methods are used.
+			case 'Expr_MethodCall':
+				$method = new \WP_Parser\Method_Call_Reflector( $node, $this->context );
 
-						$hook = new \WP_Parser\Hook_Reflector( $node, $this->context );
+				/*
+				 * If the method call is in the global scope, add it to the
+				 * file's method calls. Otherwise, add it to the queue so it
+				 * can be added to the correct node when we leave it.
+				 */
+				if ( $this === $this->getLocation() ) {
+					$this->uses['methods'][] = $method;
+				} else {
+					$this->uses_queue['methods'][] = $method;
+				}
+				break;
 
-						/*
-						 * If the hook is in the global scope, add it to the file's
-						 * hooks. Otherwise, add it to the queue so it can be added to
-						 * the correct node when we leave it.
-						 */
-						if ( $this === $this->getLocation() ) {
-							$this->uses['hooks'][] = $hook;
-						} else {
-							$this->uses_queue['hooks'][] = $hook;
-						}
-					}
-					break;
+			// Parse out method calls, so we can export where methods are used.
+			case 'Expr_StaticCall':
+				$method = new \WP_Parser\Static_Method_Call_Reflector( $node, $this->context );
 
-				// Parse out method calls, so we can export where methods are used.
-				case 'Expr_MethodCall':
-					$method = new \WP_Parser\Method_Call_Reflector( $node, $this->context );
-
-					/*
-					 * If the method call is in the global scope, add it to the
-					 * file's method calls. Otherwise, add it to the queue so it
-					 * can be added to the correct node when we leave it.
-					 */
-					if ( $this === $this->getLocation() ) {
-						$this->uses['methods'][] = $method;
-					} else {
-						$this->uses_queue['methods'][] = $method;
-					}
-					break;
-
-				// Parse out method calls, so we can export where methods are used.
-				case 'Expr_StaticCall':
-					$method = new \WP_Parser\Static_Method_Call_Reflector( $node, $this->context );
-
-					/*
-					 * If the method call is in the global scope, add it to the
-					 * file's method calls. Otherwise, add it to the queue so it
-					 * can be added to the correct node when we leave it.
-					 */
-					if ( $this === $this->getLocation() ) {
-						$this->uses['methods'][] = $method;
-					} else {
-						$this->uses_queue['methods'][] = $method;
-					}
-					break;
-			}
-
-			// Pick up DocBlock from non-documentable elements so that it can be assigned
-			// to the next hook if necessary.
-			if ( ! $this->isNodeDocumentable( $node ) && ( $docblock = $node->getDocComment() ) ) {
-				$this->last_doc = $docblock;
-			}
+				/*
+				 * If the method call is in the global scope, add it to the
+				 * file's method calls. Otherwise, add it to the queue so it
+				 * can be added to the correct node when we leave it.
+				 */
+				if ( $this === $this->getLocation() ) {
+					$this->uses['methods'][] = $method;
+				} else {
+					$this->uses_queue['methods'][] = $method;
+				}
+				break;
 		}
 
-		/**
-		 * Assign queued hooks to functions and update the node stack on leaving a node.
-		 *
-		 * We can now access the function/method reflectors, so we can assign any queued
-		 * hooks to them. The reflector for a node isn't created until the node is left.
-		 */
-		public function leaveNode( \PHPParser_Node $node ) {
-
-			parent::leaveNode( $node );
-
-			switch ( $node->getType() ) {
-				case 'Stmt_Class':
-					$class = end( $this->classes );
-					if ( ! empty( $this->method_uses_queue ) ) {
-						foreach ( $class->getMethods() as $method ) {
-							if ( isset( $this->method_uses_queue[ $method->getName() ] ) ) {
-								if ( isset( $this->method_uses_queue[ $method->getName() ]['methods'] ) ) {
-									/*
-									 * For methods used in a class, set the class on the method call.
-									 * That allows us to later get the correct class name for $this, self, parent.
-									 */
-									foreach ( $this->method_uses_queue[ $method->getName() ]['methods'] as $method_call ) {
-										$method_call->set_class( $class );
-									}
-								}
-
-								$method->uses = $this->method_uses_queue[ $method->getName() ];
-							}
-						}
-					}
-
-					$this->method_uses_queue = array();
-					array_pop( $this->location );
-					break;
-
-				case 'Stmt_Function':
-					end( $this->functions )->uses = $this->uses_queue;
-					$this->uses_queue = array();
-					array_pop( $this->location );
-					break;
-
-				case 'Stmt_ClassMethod':
-					/*
-					 * Store the list of elements used by this method in the queue. We'll
-					 * assign them to the method upon leaving the class (see above).
-					 */
-					if ( ! empty( $this->uses_queue ) ) {
-						$this->method_uses_queue[ $node->name ] = $this->uses_queue;
-						$this->uses_queue = array();
-					}
-
-					array_pop( $this->location );
-					break;
-			}
-		}
-
-		protected function isFilter( \PHPParser_Node $node ) {
-			// Ignore variable functions
-			if ( $node->name->getType() !== 'Name' ) {
-				return false;
-			}
-
-			$calling = (string) $node->name;
-
-			$functions = array(
-				'apply_filters',
-				'apply_filters_ref_array',
-				'do_action',
-				'do_action_ref_array',
-			);
-
-			return in_array( $calling, $functions );
-		}
-
-		protected function getLocation() {
-			return empty( $this->location ) ? $this : end( $this->location );
-		}
-
-		protected function isNodeDocumentable( \PHPParser_Node $node ) {
-			return parent::isNodeDocumentable( $node )
-			       || ( $node instanceof \PHPParser_Node_Expr_FuncCall
-			            && $this->isFilter( $node ) );
+		// Pick up DocBlock from non-documentable elements so that it can be assigned
+		// to the next hook if necessary.
+		if ( ! $this->isNodeDocumentable( $node ) && ( $docblock = $node->getDocComment() ) ) {
+			$this->last_doc = $docblock;
 		}
 	}
+
+	/**
+	 * Assign queued hooks to functions and update the node stack on leaving a node.
+	 *
+	 * We can now access the function/method reflectors, so we can assign any queued
+	 * hooks to them. The reflector for a node isn't created until the node is left.
+	 */
+	public function leaveNode( \PHPParser_Node $node ) {
+
+		parent::leaveNode( $node );
+
+		switch ( $node->getType() ) {
+			case 'Stmt_Class':
+				$class = end( $this->classes );
+				if ( ! empty( $this->method_uses_queue ) ) {
+					foreach ( $class->getMethods() as $method ) {
+						if ( isset( $this->method_uses_queue[ $method->getName() ] ) ) {
+							if ( isset( $this->method_uses_queue[ $method->getName() ]['methods'] ) ) {
+								/*
+								 * For methods used in a class, set the class on the method call.
+								 * That allows us to later get the correct class name for $this, self, parent.
+								 */
+								foreach ( $this->method_uses_queue[ $method->getName() ]['methods'] as $method_call ) {
+									$method_call->set_class( $class );
+								}
+							}
+
+							$method->uses = $this->method_uses_queue[ $method->getName() ];
+						}
+					}
+				}
+
+				$this->method_uses_queue = array();
+				array_pop( $this->location );
+				break;
+
+			case 'Stmt_Function':
+				end( $this->functions )->uses = $this->uses_queue;
+				$this->uses_queue = array();
+				array_pop( $this->location );
+				break;
+
+			case 'Stmt_ClassMethod':
+				/*
+				 * Store the list of elements used by this method in the queue. We'll
+				 * assign them to the method upon leaving the class (see above).
+				 */
+				if ( ! empty( $this->uses_queue ) ) {
+					$this->method_uses_queue[ $node->name ] = $this->uses_queue;
+					$this->uses_queue = array();
+				}
+
+				array_pop( $this->location );
+				break;
+		}
+	}
+
+	protected function isFilter( \PHPParser_Node $node ) {
+		// Ignore variable functions
+		if ( $node->name->getType() !== 'Name' ) {
+			return false;
+		}
+
+		$calling = (string) $node->name;
+
+		$functions = array(
+			'apply_filters',
+			'apply_filters_ref_array',
+			'do_action',
+			'do_action_ref_array',
+		);
+
+		return in_array( $calling, $functions );
+	}
+
+	protected function getLocation() {
+		return empty( $this->location ) ? $this : end( $this->location );
+	}
+
+	protected function isNodeDocumentable( \PHPParser_Node $node ) {
+		return parent::isNodeDocumentable( $node )
+		       || ( $node instanceof \PHPParser_Node_Expr_FuncCall
+		            && $this->isFilter( $node ) );
+	}
+}
--- lib/class-method-call-reflector.php
+++ lib/class-method-call-reflector.php
@@ -1,122 +1,122 @@
 <?php
 
+/**
+ * A reflection class for a method call.
+ */
+
+namespace WP_Parser;
+
+use phpDocumentor\Reflection\BaseReflector;
+
+/**
+ * A reflection of a method call expression.
+ */
+class Method_Call_Reflector extends BaseReflector {
+
 	/**
-	 * A reflection class for a method call.
+	 * The class that this method was called in, if it was called in a class.
+	 *
+	 * @var \phpDocumentor\Reflection\ClassReflector|false
 	 */
-
-	namespace WP_Parser;
-
-	use phpDocumentor\Reflection\BaseReflector;
+	protected $called_in_class = false;
 
 	/**
-	 * A reflection of a method call expression.
+	 * Returns the name for this Reflector instance.
+	 *
+	 * @return string[] Index 0 is the calling instance, 1 is the method name.
 	 */
-	class Method_Call_Reflector extends BaseReflector {
+	public function getName() {
+		$name = $this->getShortName();
 
-		/**
-		 * The class that this method was called in, if it was called in a class.
-		 *
-		 * @var \phpDocumentor\Reflection\ClassReflector|false
-		 */
-		protected $called_in_class = false;
+		$printer = new Pretty_Printer;
+		$caller = $printer->prettyPrintExpr( $this->node->var );
 
-		/**
-		 * Returns the name for this Reflector instance.
-		 *
-		 * @return string[] Index 0 is the calling instance, 1 is the method name.
-		 */
-		public function getName() {
-			$name = $this->getShortName();
+		if ( $this->called_in_class && '$this' === $caller ) {
+			$caller = $this->called_in_class->getShortName();
+		} else {
 
-			$printer = new Pretty_Printer;
-			$caller = $printer->prettyPrintExpr( $this->node->var );
+			// If the caller is a function, convert it to the function name
+			if ( is_a( $caller, 'PHPParser_Node_Expr_FuncCall' ) ) {
 
-			if ( $this->called_in_class && '$this' === $caller ) {
-				$caller = $this->called_in_class->getShortName();
-			} else {
-
-				// If the caller is a function, convert it to the function name
-				if ( is_a( $caller, 'PHPParser_Node_Expr_FuncCall' ) ) {
-
-					// Add parentheses to signify this is a function call
-					$caller = $caller->name->parts[0] . '()';
-				}
-
-				$class_mapping = $this->_getClassMapping();
-				if ( array_key_exists( $caller, $class_mapping ) ) {
-					$caller = $class_mapping[ $caller ];
-				}
+				// Add parentheses to signify this is a function call
+				$caller = $caller->name->parts[0] . '()';
 			}
 
-			return array( $caller, $name );
+			$class_mapping = $this->_getClassMapping();
+			if ( array_key_exists( $caller, $class_mapping ) ) {
+				$caller = $class_mapping[ $caller ];
+			}
 		}
 
-		/**
-		 * Set the class that this method was called within.
-		 *
-		 * @param \phpDocumentor\Reflection\ClassReflector $class
-		 */
-		public function set_class( \phpDocumentor\Reflection\ClassReflector $class ) {
-
-			$this->called_in_class = $class;
-		}
-
-		/**
-		 * Returns whether or not this method call is a static call
-		 *
-		 * @return bool Whether or not this method call is a static call
-		 */
-		public function isStatic() {
-			return false;
-		}
-
-		/**
-		 * Returns a mapping from variable names to a class name, leverages globals for most used classes
-		 *
-		 * @return array Class mapping to map variable names to classes
-		 */
-		protected function _getClassMapping() {
-
-			// List of global use generated using following command:
-			// ack "global \\\$[^;]+;" --no-filename | tr -d '\t' | sort | uniq | sed "s/global //g" | sed "s/, /,/g" | tr , '\n' | sed "s/;//g" | sort | uniq | sed "s/\\\$//g" | sed "s/[^ ][^ ]*/'&' => ''/g"
-			// There is probably an easier way, there are currently no globals that are classes starting with an underscore
-			$wp_globals = array(
-				'authordata' => 'WP_User',
-				'custom_background' => 'Custom_Background',
-				'custom_image_header' => 'Custom_Image_Header',
-				'phpmailer' => 'PHPMailer',
-				'post' => 'WP_Post',
-				'userdata' => 'WP_User', // This can also be stdClass, but you can't call methods on an stdClass
-				'wp' => 'WP',
-				'wp_admin_bar' => 'WP_Admin_Bar',
-				'wp_customize' => 'WP_Customize_Manager',
-				'wp_embed' => 'WP_Embed',
-				'wp_filesystem' => 'WP_Filesystem',
-				'wp_hasher' => 'PasswordHash', // This can be overridden by plugins, for core assume this is ours
-				'wp_json' => 'Services_JSON',
-				'wp_list_table' => 'WP_List_Table', // This one differs because there are a lot of different List Tables, assume they all only overwrite existing functions on WP_List_Table
-				'wp_locale' => 'WP_Locale',
-				'wp_object_cache' => 'WP_Object_Cache',
-				'wp_query' => 'WP_Query',
-				'wp_rewrite' => 'WP_Rewrite',
-				'wp_roles' => 'WP_Roles',
-				'wp_scripts' => 'WP_Scripts',
-				'wp_styles' => 'WP_Styles',
-				'wp_the_query' => 'WP_Query',
-				'wp_widget_factory' => 'WP_Widget_Factory',
-				'wp_xmlrpc_server' => 'wp_xmlrpc_server', // This can be overridden by plugins, for core assume this is ours
-				'wpdb' => 'wpdb',
-			);
-
-			$wp_functions = array(
-				'get_current_screen()' => 'WP_Screen',
-				'_get_list_table()' => 'WP_List_Table', // This one differs because there are a lot of different List Tables, assume they all only overwrite existing functions on WP_List_Table
-				'wp_get_theme()' => 'WP_Theme',
-			);
-
-			$class_mapping = array_merge( $wp_globals, $wp_functions );
-
-			return $class_mapping;
-		}
-
+		return array( $caller, $name );
 	}
+
+	/**
+	 * Set the class that this method was called within.
+	 *
+	 * @param \phpDocumentor\Reflection\ClassReflector $class
+	 */
+	public function set_class( \phpDocumentor\Reflection\ClassReflector $class ) {
+
+		$this->called_in_class = $class;
+	}
+
+	/**
+	 * Returns whether or not this method call is a static call
+	 *
+	 * @return bool Whether or not this method call is a static call
+	 */
+	public function isStatic() {
+		return false;
+	}
+
+	/**
+	 * Returns a mapping from variable names to a class name, leverages globals for most used classes
+	 *
+	 * @return array Class mapping to map variable names to classes
+	 */
+	protected function _getClassMapping() {
+
+		// List of global use generated using following command:
+		// ack "global \\\$[^;]+;" --no-filename | tr -d '\t' | sort | uniq | sed "s/global //g" | sed "s/, /,/g" | tr , '\n' | sed "s/;//g" | sort | uniq | sed "s/\\\$//g" | sed "s/[^ ][^ ]*/'&' => ''/g"
+		// There is probably an easier way, there are currently no globals that are classes starting with an underscore
+		$wp_globals = array(
+			'authordata' => 'WP_User',
+			'custom_background' => 'Custom_Background',
+			'custom_image_header' => 'Custom_Image_Header',
+			'phpmailer' => 'PHPMailer',
+			'post' => 'WP_Post',
+			'userdata' => 'WP_User', // This can also be stdClass, but you can't call methods on an stdClass
+			'wp' => 'WP',
+			'wp_admin_bar' => 'WP_Admin_Bar',
+			'wp_customize' => 'WP_Customize_Manager',
+			'wp_embed' => 'WP_Embed',
+			'wp_filesystem' => 'WP_Filesystem',
+			'wp_hasher' => 'PasswordHash', // This can be overridden by plugins, for core assume this is ours
+			'wp_json' => 'Services_JSON',
+			'wp_list_table' => 'WP_List_Table', // This one differs because there are a lot of different List Tables, assume they all only overwrite existing functions on WP_List_Table
+			'wp_locale' => 'WP_Locale',
+			'wp_object_cache' => 'WP_Object_Cache',
+			'wp_query' => 'WP_Query',
+			'wp_rewrite' => 'WP_Rewrite',
+			'wp_roles' => 'WP_Roles',
+			'wp_scripts' => 'WP_Scripts',
+			'wp_styles' => 'WP_Styles',
+			'wp_the_query' => 'WP_Query',
+			'wp_widget_factory' => 'WP_Widget_Factory',
+			'wp_xmlrpc_server' => 'wp_xmlrpc_server', // This can be overridden by plugins, for core assume this is ours
+			'wpdb' => 'wpdb',
+		);
+
+		$wp_functions = array(
+			'get_current_screen()' => 'WP_Screen',
+			'_get_list_table()' => 'WP_List_Table', // This one differs because there are a lot of different List Tables, assume they all only overwrite existing functions on WP_List_Table
+			'wp_get_theme()' => 'WP_Theme',
+		);
+
+		$class_mapping = array_merge( $wp_globals, $wp_functions );
+
+		return $class_mapping;
+	}
+
+}
