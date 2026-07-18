844f2dd0aa0333c29ab8bee0d55c6c651057951d
Adam Zieliński <adam@adamziel.com>
2026-07-18 22:17:40 +0200

    Validate snippet metadata and inherited setups

--- tests/phpunit/tests/import/file.php
+++ tests/phpunit/tests/import/file.php
@@ -135,4 +135,40 @@ class File_Import_Test extends Import_UnitTestCase {
 			, get_post_meta( $post->ID, '_wp-parser_tags', true )
 		);
 	}
+
+	/**
+	 * Test that snippet metadata is stored and cleared on later imports.
+	 */
+	public function test_function_snippet_metadata_imported_and_cleared() {
+
+		$posts = get_posts(
+			array( 'post_type' => $this->importer->post_type_function )
+		);
+		$post  = $posts[0];
+
+		$function_data = $this->export_data['functions'][0];
+		$snippets      = array(
+			array(
+				'type' => 'php-code-snippet',
+				'code' => '<?php echo "imported";',
+				'blueprint' => 'shared',
+			),
+		);
+		$setup_blueprints = array(
+			'shared' => array( 'steps' => array() ),
+		);
+		$function_data['doc']['code_snippets']    = $snippets;
+		$function_data['doc']['setup_blueprints'] = $setup_blueprints;
+
+		$this->importer->import_function( $function_data );
+
+		$this->assertEquals( $snippets, get_post_meta( $post->ID, '_wp-parser_code_snippets', true ) );
+		$this->assertEquals( $setup_blueprints, get_post_meta( $post->ID, '_wp-parser_setup_blueprints', true ) );
+
+		unset( $function_data['doc']['code_snippets'], $function_data['doc']['setup_blueprints'] );
+		$this->importer->import_function( $function_data );
+
+		$this->assertEquals( array(), get_post_meta( $post->ID, '_wp-parser_code_snippets', true ) );
+		$this->assertEquals( array(), get_post_meta( $post->ID, '_wp-parser_setup_blueprints', true ) );
+	}
 }
