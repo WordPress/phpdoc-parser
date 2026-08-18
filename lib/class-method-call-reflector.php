c5ff5299190b397dfb2ad80f5c1c5140ae7cbe76
Jon Surrell <sirreal@users.noreply.github.com>
2026-08-18 18:28:57 +0400

    Map global variable receivers to their classes

--- lib/class-method-call-reflector.php
+++ lib/class-method-call-reflector.php
@@ -89,35 +89,37 @@ class Method_Call_Reflector extends BaseReflector {
 	 */
 	protected function _getClassMapping() {
 
+		// Keys are receiver expressions as printed by the pretty printer, so
+		// global variables keep their `$` sigil.
 		// List of global use generated using following command:
-		// ack "global \\\$[^;]+;" --no-filename | tr -d '\t' | sort | uniq | sed "s/global //g" | sed "s/, /,/g" | tr , '\n' | sed "s/;//g" | sort | uniq | sed "s/\\\$//g" | sed "s/[^ ][^ ]*/'&' => ''/g"
+		// ack "global \\\$[^;]+;" --no-filename | tr -d '\t' | sort | uniq | sed "s/global //g" | sed "s/, /,/g" | tr , '\n' | sed "s/;//g" | sort | uniq | sed "s/[^ ][^ ]*/'&' => ''/g"
 		// There is probably an easier way, there are currently no globals that are classes starting with an underscore
 		$wp_globals = array(
-			'authordata' => 'WP_User',
-			'custom_background' => 'Custom_Background',
-			'custom_image_header' => 'Custom_Image_Header',
-			'phpmailer' => 'PHPMailer',
-			'post' => 'WP_Post',
-			'userdata' => 'WP_User', // This can also be stdClass, but you can't call methods on an stdClass
-			'wp' => 'WP',
-			'wp_admin_bar' => 'WP_Admin_Bar',
-			'wp_customize' => 'WP_Customize_Manager',
-			'wp_embed' => 'WP_Embed',
-			'wp_filesystem' => 'WP_Filesystem',
-			'wp_hasher' => 'PasswordHash', // This can be overridden by plugins, for core assume this is ours
-			'wp_json' => 'Services_JSON',
-			'wp_list_table' => 'WP_List_Table', // This one differs because there are a lot of different List Tables, assume they all only overwrite existing functions on WP_List_Table
-			'wp_locale' => 'WP_Locale',
-			'wp_object_cache' => 'WP_Object_Cache',
-			'wp_query' => 'WP_Query',
-			'wp_rewrite' => 'WP_Rewrite',
-			'wp_roles' => 'WP_Roles',
-			'wp_scripts' => 'WP_Scripts',
-			'wp_styles' => 'WP_Styles',
-			'wp_the_query' => 'WP_Query',
-			'wp_widget_factory' => 'WP_Widget_Factory',
-			'wp_xmlrpc_server' => 'wp_xmlrpc_server', // This can be overridden by plugins, for core assume this is ours
-			'wpdb' => 'wpdb',
+			'$authordata' => 'WP_User',
+			'$custom_background' => 'Custom_Background',
+			'$custom_image_header' => 'Custom_Image_Header',
+			'$phpmailer' => 'PHPMailer',
+			'$post' => 'WP_Post',
+			'$userdata' => 'WP_User', // This can also be stdClass, but you can't call methods on an stdClass
+			'$wp' => 'WP',
+			'$wp_admin_bar' => 'WP_Admin_Bar',
+			'$wp_customize' => 'WP_Customize_Manager',
+			'$wp_embed' => 'WP_Embed',
+			'$wp_filesystem' => 'WP_Filesystem',
+			'$wp_hasher' => 'PasswordHash', // This can be overridden by plugins, for core assume this is ours
+			'$wp_json' => 'Services_JSON',
+			'$wp_list_table' => 'WP_List_Table', // This one differs because there are a lot of different List Tables, assume they all only overwrite existing functions on WP_List_Table
+			'$wp_locale' => 'WP_Locale',
+			'$wp_object_cache' => 'WP_Object_Cache',
+			'$wp_query' => 'WP_Query',
+			'$wp_rewrite' => 'WP_Rewrite',
+			'$wp_roles' => 'WP_Roles',
+			'$wp_scripts' => 'WP_Scripts',
+			'$wp_styles' => 'WP_Styles',
+			'$wp_the_query' => 'WP_Query',
+			'$wp_widget_factory' => 'WP_Widget_Factory',
+			'$wp_xmlrpc_server' => 'wp_xmlrpc_server', // This can be overridden by plugins, for core assume this is ours
+			'$wpdb' => 'wpdb',
 		);
 
 		$wp_functions = array(
